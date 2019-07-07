712767079087a8420736f4eba464e938d75f6472
wutianc <2439911058@qq.com>
2019-07-07 17:02:33 +0800

    123

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+    <div class="h_header">
+      <ul>
+        <li>首页</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>   
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
